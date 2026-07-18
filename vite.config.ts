c1985a5e797b36a83c49df168f8b5503b4bfb0b4
John Söderberg <johsod96@gmail.com>
2026-07-18 02:57:27 +0200

    Add svelte-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelteSitemap } from 'svelte-sitemap/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit()],
+	plugins: [
+		enhancedImages(),
+		sveltekit(),
+		svelteSitemap({
+			domain: 'https://mariestadrockfest.se',
+			ignore: ['404.html'],
+			attribution: false
+		})
+	],
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
